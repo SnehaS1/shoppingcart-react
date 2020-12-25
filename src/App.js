029608b668f8a369eae9ffce010a15b412041e92
snehasubhash <ssubhash@prologis.com>
2020-12-25 18:53:45 +0530

    Filter Sort Changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react"; 
+import Filter from "./Components/Filter";
 import Products from "./Components/Products";
 import data from "./data.json";
 
@@ -11,6 +12,33 @@ class App extends React.Component{
       sort: ""
     }
   }
+  sortProducts(event) {
+    const sort = event.target.value;
+    console.log(sort, "lkklkl");
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products.slice()
+      .sort((a,b) => 
+        sort === "lowest" ? 
+        a.price > b.price ? 1:-1
+         : sort === "highest"
+          ?  a.price < b.price ? 1: -1
+          :a._id < b._id ? 1:-1
+      )
+
+
+    }))
+  }
+  filterProducts (event) {
+    if(event.target.value === ""){
+      this.setState({size: event.target.value, products: data.products})
+    }else{
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(product=> product.availableSizes.indexOf(event.target.value)>=0)
+      })
+    }
+  }
   render(){
     return (
       <div className="grid-container">
@@ -21,6 +49,7 @@ class App extends React.Component{
           Product List
           <div className="content">
             <div className="main">
+              <Filter count={this.state.products.length} sort ={this.state.sort} filterProducts={this.filterProducts.bind(this)} sortproduct={this.sortProducts.bind(this)} size={this.state.size}></Filter>
               <Products products={this.state.products}></Products>
               </div>
             <div className="sidebar">Cart Items</div>
